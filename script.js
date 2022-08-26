b56af3b2ea0750b169ceb07f06a2668cb008f95a
JoshBennett793 <joshbennett793@yahoo.com>
2022-08-26 12:32:10 -0600

    Add remove and read button event listeners
    Removes corresponding object from array and updates DOM. Changes read
    obj value based on current value and updates DOM accordingly.

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let modalDiv = document.createElement("div");
 const addBookBtn = document.querySelector("#addBookBtn");
 const addBookModal = document.querySelector(".addBookModal");
 const cardContainer = document.querySelector(".card-container");
-const cardContainerChildren = document.querySelector(".card-container").childNodes;
+const cardContainerChildren =
+  document.querySelector(".card-container").childNodes;
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
@@ -12,14 +13,21 @@ const readCheckbox = document.querySelector("#read");
 const submitBtn = document.querySelector(".submit");
 
 function toggleModal() {
-	if (addBookModal.style.visibility == "hidden") {
+  if (addBookModal.style.visibility == "hidden") {
     addBookModal.style.visibility = "visible";
     modalDiv.classList.add("modal-body-overlay");
     document.body.appendChild(modalDiv);
   } else {
-		addBookModal.style.visibility = "hidden";
+    addBookModal.style.visibility = "hidden";
     modalDiv.remove();
   }
+}
+
+function emptyModal() {
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readCheckbox.checked = false;
 }
 
 addBookBtn.addEventListener("click", toggleModal);
@@ -27,23 +35,15 @@ modalDiv.addEventListener("click", toggleModal);
 
 const book = {
   init: function (title, author, pages, read) {
-		this.title = title;
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     return this;
   },
-  info: function () {
-		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  },
 };
 
-function addBookToLibrary(book) {
-	myLibrary.push(book);
-}
-
 function createCard(book) {
-	createCard.prototype = Object.create(book);
   const bookCard = document.createElement("div");
   bookCard.classList.add("book-card");
   cardContainer.appendChild(bookCard);
@@ -60,50 +60,90 @@ function createCard(book) {
   pagesP.innerText = `${book.pages} pages`;
   bookCard.appendChild(pagesP);
   if (book.read === true) {
-		const readBtn = document.createElement("button");
-    readBtn.classList.add("btn", "read");
+    const readBtn = document.createElement("button");
+    readBtn.classList.add("btn", "read", "read-btn");
     readBtn.innerText = "Read";
     bookCard.appendChild(readBtn);
   } else if (book.read === false) {
-		const readBtn = document.createElement("button");
-    readBtn.classList.add("btn", "unread");
+    const readBtn = document.createElement("button");
+    readBtn.classList.add("btn", "unread", "read-btn");
     readBtn.innerText = "Unread";
     bookCard.appendChild(readBtn);
   }
+
   const removeBtn = document.createElement("button");
   removeBtn.classList.add("btn", "remove");
   removeBtn.innerText = "Remove";
   bookCard.appendChild(removeBtn);
 }
 
-function postBooksToDOM() {
-	cardContainerChildren.forEach(card => card.remove());
-  myLibrary.forEach(createCard);
+cardContainer.addEventListener("click", removeCard);
+
+function removeCard(event) {
+  let element = event.target;
+  let elIndex = element.closest(".book-card").dataset.index;
+  if (element.classList.contains("remove")) {
+    for (const [i] of myLibrary.entries()) {
+      if (i == elIndex) {
+        myLibrary.splice(i, 1);
+        postBooksToDOM();
+      }
+    }
+  }
 }
 
-function emptyModal() {
-	titleInput.value = "";
-	authorInput.value = "";
-	pagesInput.value = "";
-	readCheckbox.checked = false;
+cardContainer.addEventListener("click", changeReadStatus);
+
+function changeReadStatus(event) {
+  let element = event.target;
+  let elIndex = element.closest(".book-card").dataset.index;
+  if (element.classList.contains("read-btn")) {
+    for (const [i] of myLibrary.entries()) {
+      if (i == elIndex) {
+        if (element.classList.contains("read")) {
+          myLibrary[i].read = false;
+        } else {
+          myLibrary[i].read = true;
+        }
+        postBooksToDOM();
+      }
+    }
+  }
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function setDataAttr() {
+  const card = document.querySelectorAll(".book-card");
+  card.forEach((element, index) => {
+    element.setAttribute("data-index", index);
+  });
+}
+
+function postBooksToDOM() {
+  cardContainer.innerHTML = "";
+  myLibrary.forEach((book) => {
+    createCard(book);
+    setDataAttr();
+  });
 }
 
 submitBtn.addEventListener("click", () => {
-	if (readCheckbox.checked) {
-		read = true;
-	} else {
-		read = false;
-	}
-	const newBook = Object.create(book).init(
-		titleInput.value,
-		authorInput.value,
-		pagesInput.value,
-		read
-		);
-		emptyModal();
-		toggleModal();
-		addBookToLibrary(newBook);
-		postBooksToDOM();
-		console.log(cardContainerChildren);
-	});
-	
+  if (readCheckbox.checked) {
+    read = true;
+  } else {
+    read = false;
+  }
+  const newBook = Object.create(book).init(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    read
+  );
+  emptyModal();
+  toggleModal();
+  addBookToLibrary(newBook);
+  postBooksToDOM();
+});
